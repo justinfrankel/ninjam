5638ac7bbfc5ee79b049ed1141f98be595d86fd3
Cockos Inc <nospam@cockos.com>
2005-05-29 23:36:15 +0000

    Vss2Git -- from 88783747

--- ninjam/mpb.cpp
+++ ninjam/mpb.cpp
@@ -3,6 +3,8 @@
 #include "mpb.h"
 
 
+
+// MESSAGE_SERVER_AUTH_CHALLENGE 
 int mpb_server_auth_challenge::parse(Net_Message *msg) // return 0 on success
 {
   if (msg->get_type() != MESSAGE_SERVER_AUTH_CHALLENGE) return -1;
@@ -65,6 +67,8 @@ Net_Message *mpb_server_auth_challenge::build()
 }
 
 
+
+// MESSAGE_SERVER_AUTH_REPLY
 int mpb_server_auth_reply::parse(Net_Message *msg) // return 0 on success
 {
   if (msg->get_type() != MESSAGE_SERVER_AUTH_REPLY) return -1;
@@ -93,6 +97,46 @@ Net_Message *mpb_server_auth_reply::build()
   }
 
   *p=flag;
+
+  return nm;
+}
+
+
+// MESSAGE_SERVER_CONFIG_CHANGE_NOTIFY
+int mpb_server_config_change_notify::parse(Net_Message *msg) // return 0 on success
+{
+  if (msg->get_type() != MESSAGE_SERVER_CONFIG_CHANGE_NOTIFY) return -1;
+  if (msg->get_size() < 4) return 1;
+  unsigned char *p=(unsigned char *)msg->get_data();
+  if (!p) return 2;
+
+  beats_minute = *p++;
+  beats_minute |= ((int)*p++)<<8;
+  beats_interval = *p++;
+  beats_interval |= ((int)*p++)<<8;
+
+  return 0;
+}
+
+Net_Message *mpb_server_config_change_notify::build()
+{
+  Net_Message *nm=new Net_Message;
+  nm->set_type(MESSAGE_SERVER_CONFIG_CHANGE_NOTIFY);
+  
+  nm->set_size(4);
+
+  unsigned char *p=(unsigned char *)nm->get_data();
+
+  if (!p)
+  {
+    delete nm;
+    return 0;
+  }
+
+  *p++=beats_minute&0xff;
+  *p++=(beats_minute>>8)&0xff;
+  *p++=beats_interval&0xff;
+  *p++=(beats_interval>>8)&0xff;
 
   return nm;
 }
--- ninjam/mpb.h
+++ ninjam/mpb.h
@@ -41,7 +41,28 @@ class mpb_server_auth_reply
 
 
 #define MESSAGE_SERVER_CONFIG_CHANGE_NOTIFY 0x02
+
+class mpb_server_config_change_notify
+{
+  public:
+    mpb_server_config_change_notify() : beats_minute(120), beats_interval(32) { }
+    ~mpb_server_config_change_notify() { }
+
+    int parse(Net_Message *msg); // return 0 on success
+    Net_Message *build();
+
+
+    // public data
+    int beats_minute;  //bpm
+    int beats_interval;  // beats/interval
+};
+
+
+
 #define MESSAGE_SERVER_USERLIST_CHANGE_NOTIFY 0x03
+
+
+
 #define MESSAGE_SERVER_DOWNLOAD_INTERVAL_BEGIN 0x04
 #define MESSAGE_SERVER_DOWNLOAD_INTERVAL_WRITE 0x05
 
