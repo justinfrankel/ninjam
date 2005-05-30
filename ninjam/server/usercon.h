76a6213b45e55b46f88656210afa1984e422612b
Cockos Inc <nospam@cockos.com>
2005-05-30 17:32:37 +0000

    Vss2Git -- from 169dbf0a

--- ninjam/server/usercon.h
+++ ninjam/server/usercon.h
@@ -3,20 +3,48 @@
 
 
 #include "../netmsg.h"
-
+#include "../../WDL/string.h"
 
 #define MAX_USER_CHANNELS 32
+#define MAX_USERS 64
+#define MAX_UPLOADS 32
+#define MAX_DOWNLOADS 128
 
+class User_SubscribeMask
+{
+public:
+  User_SubscribeMask();
+  ~User_SubscribeMask();
+  WDL_String username;
+  unsigned int channelmask;
+};
 
 class User_Channel
 {
 public:
-  User_Channel() { name=0; volume=0; panning=128; }
-  ~User_Channel() { free(name); }
-  char *name;
+  User_Channel() { volume=0; panning=128; }
+  ~User_Channel() { }
+  WDL_String name;
   int volume;
   int panning;
 };
+
+class User_TransferState
+{
+public:
+  User_TransferState() { }
+  ~User_TransferState() { }
+
+  unsigned int bytes_sofar;
+  unsigned int bytes_estimated;
+  unsigned int fourcc;
+  
+  FILE *fp;
+
+
+};
+
+
 
 class User_Connection
 {
@@ -35,7 +63,11 @@ class User_Connection
 
 
     int m_num_channels;
-    User_Channel m_channels[32];
+    User_Channel m_channels[MAX_USER_CHANNELS];
+    User_SubscribeMask m_subscribemask[MAX_USERS];
+
+    User_TransferState m_uploads[MAX_UPLOADS];
+    User_TransferState m_downloads[MAX_DOWNLOADS];
 
 };
 
