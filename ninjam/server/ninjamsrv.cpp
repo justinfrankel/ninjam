f4f0ce21bd8085b26524fd279dd0a6c702d67c7d
Cockos Inc <nospam@cockos.com>
2005-06-14 17:22:13 +0000

    Vss2Git -- from 9171257f

--- ninjam/server/ninjamsrv.cpp
+++ ninjam/server/ninjamsrv.cpp
@@ -327,7 +327,7 @@ int main(int argc, char **argv)
     exit(1);
   }
 
-  printf("Ninjam v0.004 server starting up...\n");
+  printf("Ninjam v0.005 server starting up...\n");
   printf("Copyright (C) 2005, Cockos, Inc.\n");
   if (ReadConfig(argv[1]))
   {
