26c797fc4516592b5665e743e2fcf835dfa2bdef
Cockos Inc <nospam@cockos.com>
2005-06-14 17:17:17 +0000

    Vss2Git -- from 65138b3d

--- ninjam/cursesclient/cursesclient.cpp
+++ ninjam/cursesclient/cursesclient.cpp
@@ -631,7 +631,7 @@ int main(int argc, char **argv)
   WDL_String sessiondir;
   int nolog=0,nowav=0;
 
-  printf("Ninjam v0.008 curses client, Copyright (C) 2004-2005 Cockos, Inc.\n");
+  printf("Ninjam v0.009 curses client, Copyright (C) 2004-2005 Cockos, Inc.\n");
   char *audioconfigstr=NULL;
   g_client=new NJClient;
   g_client->config_savelocalaudio=1;
