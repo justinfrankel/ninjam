9c4691c8a0917024374add2d583beced9b47c735
Cockos Inc <nospam@cockos.com>
2005-07-01 03:37:58 +0000

    local channel and master config saving -- from bd7321de

--- ninjam/cursesclient/cursesclient.cpp
+++ ninjam/cursesclient/cursesclient.cpp
@@ -26,6 +26,7 @@
 
 #include "../njclient.h"
 #include "../../WDL/dirscan.h"
+#include "../../WDL/lineparse.h"
 
 #ifdef _WIN32
 #include "../../jesusonic/jesusonic_dll.h"
@@ -223,6 +224,29 @@ void jesusonic_processor(float *buf, int len, void *inst)
     JesusonicAPI->osc_run(inst,(char*)buf,len);
   }
 #endif
+}
+void CreateJesusInstance(int a)
+{
+  if (JesusonicAPI)
+  {
+    void *myInst=JesusonicAPI->createInstance();
+    JesusonicAPI->set_rootdir(myInst,jesusdir);
+    JesusonicAPI->ui_init(myInst);
+    JesusonicAPI->set_opts(myInst,1,1,-1);
+    JesusonicAPI->set_sample_fmt(myInst,g_audio->m_srate,1,33);
+    JesusonicAPI->set_status(myInst,"","ninjam embedded");
+    HWND h=JesusonicAPI->ui_wnd_create(myInst);
+    ShowWindow(h,SW_SHOWNA);
+    SetTimer(h,1,40,NULL);
+    SetForegroundWindow(h);
+
+    char buf[4096];
+    sprintf(buf,"%s\\ninjam.p%02d",jesusdir?jesusdir:".",a);
+
+    JesusonicAPI->preset_load(myInst,buf);
+
+    g_client->SetLocalChannelProcessor(a,jesusonic_processor,myInst);
+  }
 }
 
 
@@ -470,26 +494,7 @@ void showmainview(bool action=false, int ymove=0)
         if (!i)
         {
           // start it up
-          if (JesusonicAPI)
-          {
-            void *myInst=JesusonicAPI->createInstance();
-            JesusonicAPI->set_rootdir(myInst,jesusdir);
-            JesusonicAPI->ui_init(myInst);
-            JesusonicAPI->set_opts(myInst,1,1,-1);
-            JesusonicAPI->set_sample_fmt(myInst,g_audio->m_srate,1,33);
-            JesusonicAPI->set_status(myInst,"","ninjam embedded");
-            HWND h=JesusonicAPI->ui_wnd_create(myInst);
-            ShowWindow(h,SW_SHOWNA);
-            SetTimer(h,1,40,NULL);
-            SetForegroundWindow(h);
-
-            char buf[4096];
-            sprintf(buf,"%s\\ninjam.p%02d",jesusdir?jesusdir:".",a);
-
-            JesusonicAPI->preset_load(myInst,buf);
-
-            g_client->SetLocalChannelProcessor(a,jesusonic_processor,myInst);
-          }
+          CreateJesusInstance(a);
         }
         else 
         {
@@ -1166,9 +1171,118 @@ int main(int argc, char **argv)
 #endif
   // end jesusonic init
 
-  g_client->SetLocalChannelInfo(0,"channel0",true,0,false,0,true,true);
-  g_client->SetLocalChannelMonitoring(0,false,0.0f,false,0.0f,false,false,false,false);
- 
+  {
+    FILE *fp=fopen("ninjam.config","rt");
+    int x=0;
+    if (fp) 
+    {
+      bool comment_state=false;
+      while (!feof(fp))
+      {
+        char buf[4096];
+        buf[0]=0;
+        fgets(buf,sizeof(buf),fp);
+        if (!buf[0]) continue;
+        if (buf[strlen(buf)-1] == '\n')
+          buf[strlen(buf)-1]=0;
+        if (!buf[0]) continue;
+
+        LineParser lp(comment_state);
+
+        lp.parse(buf);
+
+        switch (lp.gettoken_enum(0,"local\0master\0"))
+        {
+          case 0:
+            // process local line
+            if (lp.getnumtokens()>2)
+            {
+              int ch=lp.gettoken_int(1);
+              int n;
+              for (n = 2; n < lp.getnumtokens()-1; n += 2)
+              {
+                switch (lp.gettoken_enum(n,"source\0bc\0mute\0solo\0volume\0pan\0jesus\0name\0"))
+                {
+                  case 0: // source 
+                    g_client->SetLocalChannelInfo(ch,NULL,true,lp.gettoken_int(n+1),false,0,false,false);
+                  break;
+                  case 1: //broadcast
+                    g_client->SetLocalChannelInfo(ch,NULL,false,false,false,0,true,!!lp.gettoken_int(n+1));
+                  break;
+                  case 2: //mute
+                    g_client->SetLocalChannelMonitoring(ch,false,false,false,false,true,!!lp.gettoken_int(n+1),false,false);
+                  break;
+                  case 3: //solo
+                    g_client->SetLocalChannelMonitoring(ch,false,false,false,false,false,false,true,!!lp.gettoken_int(n+1));
+                  break;
+                  case 4: //volume
+                    g_client->SetLocalChannelMonitoring(ch,true,(float)lp.gettoken_float(n+1),false,false,false,false,false,false);
+                  break;
+                  case 5: //pan
+                    g_client->SetLocalChannelMonitoring(ch,false,false,true,(float)lp.gettoken_float(n+1),false,false,false,false);
+                  break;
+                  case 6: //jesus
+                    if (lp.gettoken_int(n+1))
+                    {
+                      CreateJesusInstance(ch);
+                    }
+                  break;
+                  case 7: //name
+                    g_client->SetLocalChannelInfo(ch,lp.gettoken_str(n+1),false,false,false,0,false,false);
+                  break;
+                  default:
+                  break;
+                }
+              }
+            }
+
+          break;
+          case 1:
+            if (lp.getnumtokens()>2)
+            {
+              int n;
+              for (n = 1; n < lp.getnumtokens()-1; n += 2)
+              {
+                switch (lp.gettoken_enum(n,"mastervol\0masterpan\0metrovol\0metropan\0mastermute\0metromute\0"))
+                {
+                  case 0: // mastervol
+                    g_client->config_mastervolume = (float)lp.gettoken_float(n+1);
+                  break;
+                  case 1: // masterpan
+                    g_client->config_masterpan = (float)lp.gettoken_float(n+1);
+                  break;
+                  case 2:
+                    g_client->config_metronome = (float)lp.gettoken_float(n+1);
+                  break;
+                  case 3:
+                    g_client->config_metronome_pan = (float)lp.gettoken_float(n+1);
+                  break;
+                  case 4:
+                    g_client->config_mastermute = !!lp.gettoken_int(n+1);
+                  break;
+                  case 5:
+                    g_client->config_metronome_mute = !!lp.gettoken_int(n+1);
+                  break;
+                  default:
+                  break;
+                }
+              }
+            }
+          break;
+          default:
+          break;
+        }       
+
+
+      }
+      fclose(fp);
+    }    
+    else // set up defaults
+    {
+      g_client->SetLocalChannelInfo(0,"channel0",true,0,false,0,true,true);
+      g_client->SetLocalChannelMonitoring(0,false,0.0f,false,0.0f,false,false,false,false);
+    }
+  } 
 
   if (!sessiondir.Get()[0])
   {
@@ -1722,6 +1836,47 @@ time(NULL) >= nextupd
   delete g_client->waveWrite;
   g_client->waveWrite=0;
 
+
+  // save local channel state
+  {
+    FILE *fp=fopen("ninjam.config","wt");
+    int x=0;
+    if (fp) 
+    {
+      fprintf(fp,"master mastervol %f masterpan %f metrovol %f metropan %f mastermute %d metromute %d\n",
+        g_client->config_mastervolume,g_client->config_masterpan,g_client->config_metronome,g_client->config_metronome_pan,
+        g_client->config_mastermute,g_client->config_metronome_mute);
+
+
+
+      for (x = 0;;x++)
+      {
+        int a=g_client->EnumLocalChannels(x);
+        if (a<0) break;
+
+
+        int sch=0;
+        bool bc=0;
+        void *has_jesus=0;
+        char *lcn;
+        float v=0.0f,p=0.0f;
+        bool m=0,s=0;
+      
+        lcn=g_client->GetLocalChannelInfo(a,&sch,NULL,&bc);
+        g_client->GetLocalChannelMonitoring(a,&v,&p,&m,&s);
+        g_client->GetLocalChannelProcessor(a,NULL,&has_jesus);
+
+        char *ptr=lcn;
+        while (*ptr)
+        {
+          if (*ptr == '`') *ptr='\'';
+          ptr++;
+        }
+        fprintf(fp,"local %d source %d bc %d mute %d solo %d volume %f pan %f jesus %d name `%s`\n",a,sch,bc,m,s,v,p,!!has_jesus,lcn);
+      }
+      fclose(fp);
+    }    
+  }
 
 
   // delete all effects processors in g_client
