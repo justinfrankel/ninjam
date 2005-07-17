216df3ee0a943cd63fdc9a1d291607154c1003a2
Cockos Inc <nospam@cockos.com>
2005-07-17 22:10:11 +0000

    Vss2Git -- from bc33fd18

--- ninjam/cursesclient/curses_win32.cpp
+++ ninjam/cursesclient/curses_win32.cpp
@@ -1,3 +1,29 @@
+/*
+    NINJAM Curses Client - curses_win32.cpp
+    Copyright (C) 2005 Cockos Incorporated
+
+    NINJAM is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    NINJAM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NINJAM; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+
+  This is a Win32 Curses emulation layer. Only a subset of the curses API is supported.
+  It's a bit of a kludge, I know.
+  
+  */
+
 #ifdef _WIN32
 
 
