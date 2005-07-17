27be1e7f6df829123e48a8bd1cff117dab1782ff
Cockos Inc <nospam@cockos.com>
2005-07-17 19:54:34 +0000

    Vss2Git -- from ffba9695

--- ninjam/njasiodrv/njadmain.cpp
+++ ninjam/njasiodrv/njadmain.cpp
@@ -1,13 +1,202 @@
 #include <windows.h>
 
+#include "resource.h"
 #include "../audiostream.h"
+
+
+
+typedef struct
+{
+  int asio_driver;
+  int asio_input[2];
+  int asio_output[2];
+} asio_config_type;
+
+#include "../asio/asiosys.h"
+#include "../asio/asio.h"
+#include "../asio/asiodrivers.h"
+
+extern AsioDrivers* asioDrivers;
+
+
+static BOOL CALLBACK cfgproc_asio( HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+  asio_config_type *cfg;
+  if (uMsg == WM_INITDIALOG)
+  {
+    SetWindowLong(hwndDlg,GWL_USERDATA,(long)(cfg = (asio_config_type *)lParam));
+	  if(!asioDrivers)
+		  asioDrivers = new AsioDrivers();
+
+    if (asioDrivers)
+    {
+      int l=asioDrivers->asioGetNumDev();
+      int x;
+      for (x = 0; x < l; x ++)
+      {
+        char buf[256];
+        asioDrivers->asioGetDriverName(x,buf,sizeof(buf));
+        SendDlgItemMessage(hwndDlg,IDC_COMBO1,CB_ADDSTRING,0,(LPARAM)buf);        
+      }
+      if (!l)
+        SendDlgItemMessage(hwndDlg,IDC_COMBO1,CB_ADDSTRING,0,(LPARAM)"No ASIO drivers found!");
+      int wd=cfg->asio_driver;
+      if (wd<0||wd>l) wd=0;
+      SendDlgItemMessage(hwndDlg,IDC_COMBO1,CB_SETCURSEL,(WPARAM)wd,0);
+      SendMessage(hwndDlg,WM_COMMAND,MAKEWPARAM(IDC_COMBO1,CBN_SELCHANGE),0);
+    }   
+    //delete asioDrivers; // fucko thsi shouldnt be commented
+   // asioDrivers=0;
+    
+    return 1;
+  }
+  cfg = (asio_config_type *)GetWindowLong(hwndDlg,GWL_USERDATA);
+  if (uMsg == WM_COMMAND && wParam == MAKEWPARAM(IDC_COMBO1,CBN_SELCHANGE))
+  {
+    int wp=SendDlgItemMessage(hwndDlg,IDC_COMBO1,CB_GETCURSEL,0,0);
+    SendDlgItemMessage(hwndDlg,IDC_COMBO2,CB_RESETCONTENT,0,0);
+    SendDlgItemMessage(hwndDlg,IDC_COMBO3,CB_RESETCONTENT,0,0);
+    SendDlgItemMessage(hwndDlg,IDC_COMBO4,CB_RESETCONTENT,0,0);
+    SendDlgItemMessage(hwndDlg,IDC_COMBO5,CB_RESETCONTENT,0,0);
+    if (wp != CB_ERR)
+    {
+      char buf[256];
+      SendDlgItemMessage(hwndDlg,IDC_COMBO1,CB_GETLBTEXT,wp,(LPARAM)buf);
+	    if(!asioDrivers)
+		    asioDrivers = new AsioDrivers();
+      if (asioDrivers && asioDrivers->loadDriver(buf))
+      {
+      	ASIODriverInfo driverInfo;
+        driverInfo.sysRef = (void *)hwndDlg;
+        if (ASIOInit(&driverInfo) == ASE_OK)
+        {
+          long inch=0,outch=0;
+	        if (ASIOGetChannels(&inch, &outch) == ASE_OK) 
+          {
+            long i;
+            for (i = 0; i < inch; i ++)
+            {
+              ASIOChannelInfo c;
+              c.channel=i;
+              c.isInput=1;
+              if (ASIOGetChannelInfo(&c) == ASE_OK) 
+              {
+		char buf[1024];
+		wsprintf(buf,"%d: %s",i+1,c.name);
+                SendDlgItemMessage(hwndDlg,IDC_COMBO2,CB_ADDSTRING,0,(LPARAM)buf);
+                SendDlgItemMessage(hwndDlg,IDC_COMBO4,CB_ADDSTRING,0,(LPARAM)buf);
+              }
+            }
+            for (i = 0; i < outch; i ++)
+            {
+              ASIOChannelInfo c;
+              c.channel=i;
+              c.isInput=0;
+              if (ASIOGetChannelInfo(&c) == ASE_OK) 
+              {
+		char buf[1024];
+		wsprintf(buf,"%d: %s",i+1,c.name);
+                SendDlgItemMessage(hwndDlg,IDC_COMBO3,CB_ADDSTRING,0,(LPARAM)buf);
+                SendDlgItemMessage(hwndDlg,IDC_COMBO5,CB_ADDSTRING,0,(LPARAM)buf);
+              }
+            }
+            int wd=cfg->asio_input[0];
+            if (wd<0||wd>inch) wd=0;
+            SendDlgItemMessage(hwndDlg,IDC_COMBO2,CB_SETCURSEL,(WPARAM)wd,0);
+            wd=cfg->asio_input[1];
+            if (wd<0||wd>inch) wd=0;
+            SendDlgItemMessage(hwndDlg,IDC_COMBO4,CB_SETCURSEL,(WPARAM)wd,0);
+            wd=cfg->asio_output[0];
+            if (wd<0||wd>outch) wd=0;
+            SendDlgItemMessage(hwndDlg,IDC_COMBO3,CB_SETCURSEL,(WPARAM)wd,0);
+            wd=cfg->asio_output[1];
+            if (wd<0||wd>outch) wd=0;
+            SendDlgItemMessage(hwndDlg,IDC_COMBO5,CB_SETCURSEL,(WPARAM)wd,0);
+          } 
+          ASIOExit();
+        }
+
+        asioDrivers->removeCurrentDriver();
+      }
+      //delete asioDrivers;// fucko thsi shouldnt be commented
+     // asioDrivers=0;
+    }
+    // repopulate channel list
+  }
+
+  if (uMsg == WM_COMMAND && LOWORD(wParam) == IDC_BUTTON1)
+  {
+    char buf[256];
+    int wp=SendDlgItemMessage(hwndDlg,IDC_COMBO1,CB_GETCURSEL,0,0);
+    if (wp != CB_ERR)
+    {
+      if (!asioDrivers) asioDrivers = new AsioDrivers();
+
+      SendDlgItemMessage(hwndDlg,IDC_COMBO1,CB_GETLBTEXT,wp,(LPARAM)buf);
+      if (asioDrivers && asioDrivers->loadDriver(buf))
+      {
+        ASIODriverInfo driverInfo;
+        driverInfo.sysRef=(HWND)hwndDlg;
+        if (ASIOInit(&driverInfo) == ASE_OK)
+        {
+          ASIOControlPanel();
+          ASIOExit();
+        }
+      }
+      asioDrivers->removeCurrentDriver();
+     // if (asioDrivers) delete asioDrivers;
+   //   asioDrivers=0;// fucko thsi shouldnt be commented
+    }
+  }
+
+  if (uMsg == WM_COMMAND && LOWORD(wParam) == IDOK)
+  {
+    int a;
+    a=SendDlgItemMessage(hwndDlg,IDC_COMBO1,CB_GETCURSEL,0,0);
+    if (a != CB_ERR) cfg->asio_driver = a;
+    a=SendDlgItemMessage(hwndDlg,IDC_COMBO2,CB_GETCURSEL,0,0);
+    if (a != CB_ERR) cfg->asio_input[0] = a;
+    a=SendDlgItemMessage(hwndDlg,IDC_COMBO4,CB_GETCURSEL,0,0);
+    if (a != CB_ERR) cfg->asio_input[1] = a;
+    a=SendDlgItemMessage(hwndDlg,IDC_COMBO3,CB_GETCURSEL,0,0);
+    if (a != CB_ERR) cfg->asio_output[0] = a;
+    a=SendDlgItemMessage(hwndDlg,IDC_COMBO5,CB_GETCURSEL,0,0);
+    if (a != CB_ERR) cfg->asio_output[1] = a;
+  }
+  return 0;
+}
+
+
 
 extern "C"
 {
 
+HINSTANCE m_hInst;
+
+BOOL WINAPI DllMain(
+  HINSTANCE hinstDLL,  // handle to DLL module
+  DWORD fdwReason,     // reason for calling function
+  LPVOID lpvReserved   // reserved
+)
+{
+  if (fdwReason == DLL_PROCESS_ATTACH)
+  {
+    m_hInst = hinstDLL;
+  }
+
+  return 0;
+}
+
 __declspec(dllexport) audioStreamer *create_asio_streamer(char **dev, SPLPROC proc)
 {
   return create_audioStreamer_ASIO(dev,proc);
+}
+
+
+
+__declspec(dllexport) HWND create_asio_configdlg(HWND parent, asio_config_type *cfg)
+{
+  return CreateDialogParam(m_hInst,MAKEINTRESOURCE(IDD_CFG_ASIO),parent,cfgproc_asio,(LPARAM)cfg);
 }
 
 };
--- ninjam/njasiodrv/njasiodrv.dsp
+++ ninjam/njasiodrv/njasiodrv.dsp
@@ -40,9 +40,10 @@ RSC=rc.exe
 # PROP Use_Debug_Libraries 0
 # PROP Output_Dir "Release"
 # PROP Intermediate_Dir "Release"
+# PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /MT /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "NJASIODRV_EXPORTS" /YX /FD /c
-# ADD CPP /nologo /MT /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "NJASIODRV_EXPORTS" /YX /FD /c
+# ADD CPP /nologo /MD /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "NJASIODRV_EXPORTS" /YX /FD /c
 # ADD BASE MTL /nologo /D "NDEBUG" /mktyplib203 /win32
 # ADD MTL /nologo /D "NDEBUG" /mktyplib203 /win32
 # ADD BASE RSC /l 0x409 /d "NDEBUG"
@@ -52,7 +53,8 @@ BSC32=bscmake.exe
 # ADD BSC32 /nologo
 LINK32=link.exe
 # ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /dll /machine:I386
-# ADD LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /dll /machine:I386
+# ADD LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /entry:"DllMain" /dll /machine:I386 /out:"../winclient/release/njasiodrv.dll" /opt:nowin98
+# SUBTRACT LINK32 /pdb:none
 
 !ELSEIF  "$(CFG)" == "njasiodrv - Win32 Debug"
 
@@ -67,7 +69,7 @@ LINK32=link.exe
 # PROP Intermediate_Dir "Debug"
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /MTd /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "NJASIODRV_EXPORTS" /YX /FD /GZ  /c
-# ADD CPP /nologo /MTd /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "NJASIODRV_EXPORTS" /YX /FD /GZ  /c
+# ADD CPP /nologo /MDd /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "NJASIODRV_EXPORTS" /YX /FD /GZ  /c
 # ADD BASE MTL /nologo /D "_DEBUG" /mktyplib203 /win32
 # ADD MTL /nologo /D "_DEBUG" /mktyplib203 /win32
 # ADD BASE RSC /l 0x409 /d "_DEBUG"
@@ -77,7 +79,7 @@ BSC32=bscmake.exe
 # ADD BSC32 /nologo
 LINK32=link.exe
 # ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /dll /debug /machine:I386 /pdbtype:sept
-# ADD LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /dll /debug /machine:I386 /pdbtype:sept
+# ADD LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /entry:"DllMain" /dll /debug /machine:I386 /pdbtype:sept
 
 !ENDIF 
 
@@ -120,6 +122,10 @@ SOURCE=.\njadmain.cpp
 # Begin Group "Resource Files"
 
 # PROP Default_Filter "ico;cur;bmp;dlg;rc2;rct;bin;rgs;gif;jpg;jpeg;jpe"
+# Begin Source File
+
+SOURCE=.\njasiodrvres.rc
+# End Source File
 # End Group
 # End Target
 # End Project
