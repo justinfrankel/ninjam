f8d7f9e4a0d996ba3e3c7bf8ab6a48480f0ccb7e
Cockos Inc <nospam@cockos.com>
2005-07-17 21:30:52 +0000

    Vss2Git -- from 8c7a3c92

--- ninjam/audioconfig.cpp
+++ ninjam/audioconfig.cpp
@@ -1,3 +1,31 @@
+/*
+    NINJAM - audioconfig.cpp
+    Copyright (C) 2005 Cockos Incorporated
+
+    NINJAM is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    NINJAM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NINJAM; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+
+  This file has implementation for configuring audio devices UI and helper code to 
+  actually create them (by calling create_audiostreamer_*) for the Windows client.
+
+  The mac client has its own code for doing this.
+
+*/
+
 #include <windows.h>
 #include <stdio.h>
 #include <dsound.h>
--- ninjam/audiostream.h
+++ ninjam/audiostream.h
@@ -1,3 +1,39 @@
+/*
+    NINJAM - audiostream.h
+    Copyright (C) 2005 Cockos Incorporated
+
+    NINJAM is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    NINJAM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NINJAM; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+
+  This header is used by NINJAM clients to define an abstract audio streamer interface, as
+  well as declare functions for creating instances of these audio streamers. 
+
+  On Windows, these functions are primarily called from audioconfig.cpp, and on
+  the Cocoa client the function is called from Controller.mm.
+
+  The basic structure is:
+
+  The client runs, creates an audiostreamer (below), giving it a SPLPROC, which is it's
+  own function that then in turn calls NJClient::AudioProc. 
+
+  But this is just the interface declaration etc.
+
+*/
+
 #ifndef _AUDIOSTREAM_H_
 #define _AUDIOSTREAM_H_
 
@@ -18,8 +54,6 @@ typedef void (*SPLPROC)(float **inbuf, int innch, float **outbuf, int outnch, in
 
 
 #ifdef _WIN32
-audioStreamer *create_audioStreamer_ASIO(char **dev, SPLPROC proc);
-
 audioStreamer *create_audioStreamer_KS(int srate, int bps, int *nbufs, int *bufsize, SPLPROC proc);
 
 audioStreamer *create_audioStreamer_WO(int srate, int bps, int devs[2], int *nbufs, int *bufsize, SPLPROC proc);
