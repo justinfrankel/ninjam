740603150ed2b8224e8c1ba0e6f82aabd7cc06bf
Cockos Inc <nospam@cockos.com>
2005-07-18 01:42:33 +0000

    Vss2Git -- from 7c6fb8f3

--- ninjam/njasiodrv/audiostream_asio.cpp
+++ ninjam/njasiodrv/audiostream_asio.cpp
@@ -1,3 +1,35 @@
+/*
+    NINJAM ASIO driver - audiostream_asio.cpp
+    Copyright (C) 2005 Cockos Incorporated
+
+  
+    This software is provided 'as-is', without any express or implied
+    warranty.  In no event will the authors be held liable for any damages
+    arising from the use of this software.
+
+    Permission is granted to anyone to use this software for any purpose,
+    including commercial applications, and to alter it and redistribute it
+    freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+       claim that you wrote the original software. If you use this software
+       in a product, an acknowledgment in the product documentation would be
+       appreciated but is not required.
+    2. Altered source versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.
+    3. This notice may not be removed or altered from any source distribution.
+    
+
+*/
+
+/*
+
+  This provides ASIO input/output functionality for NINJAM. The reason it is 
+  in a seperate DLL file is so that people who want to develop the GPL'd 
+  client can do so without having to agree to Steinberg's ASIO SDK license 
+  agreement.  This file implements an audioStreamer object that uses ASIO.
+*/
+
 #include <windows.h>
 #include <mmsystem.h>
 
