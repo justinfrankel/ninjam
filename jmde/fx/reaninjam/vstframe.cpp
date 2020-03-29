bed1a993193935e353a4939cb24a4d8f626fa4e8
Cockos Inc <nospam@cockos.com>
2020-03-29 12:41:18 -0400

    reaninjam: remove a bunch of vst cruft -- from 92778fbe

--- jmde/fx/reaninjam/vstframe.cpp
+++ jmde/fx/reaninjam/vstframe.cpp
@@ -27,23 +27,6 @@
 #define NUM_INPUTS 8
 #define NUM_OUTPUTS 2
 
-#define NUM_PARAMS 0
-
-#define USE_BOOL -1000.0
-#define USE_DB -10000.0
-typedef struct
-{
-   double parm_maxval;
-   const char *name;
-   const char *label;
-   double defparm;
-   double minval; // can be USE_DB,USE_BOOL
-   double maxval;
-   unsigned short ui_id_slider, ui_id_lbl;
-   int precisiondigits;
-   double slidershape;
-} parameterInfo; 
-
 DWORD g_object_allocated;
 void audiostream_onsamples(float **inbuf, int innch, float **outbuf, int outnch, int len, int srate, bool isPlaying, bool isSeek, double curpos) ;
 void InitializeInstance();
@@ -52,17 +35,6 @@ void QuitInstance();
 
 void (*format_timestr_pos)(double tpos, char *buf, int buflen, int modeoverride); // actually implemented in tracklist.cpp for now
 
-
-static double sliderscale_sq(double in, int dir, double n)
-{
-  if (dir < 0) return (pow((double) in,n)/pow(1000.0,n-1.0));
-  return (pow(in * pow(1000.0,n-1),1/n));
-}
-
-static parameterInfo param_infos[1 /*NUM_PARAMS*/]=
-{
- 0,
-};
 
 audioMasterCallback g_hostcb;
 
@@ -96,41 +68,6 @@ int (WINAPI *CoolSB_SetScrollPos)(HWND hwnd, int nBar, int nPos, BOOL fRedraw);
 int (WINAPI *CoolSB_SetScrollRange)(HWND hwnd, int nBar, int nMinPos, int nMaxPos, BOOL fRedraw);
 BOOL (WINAPI *CoolSB_SetMinThumbSize)(HWND hwnd, UINT wBar, UINT size);
 int (*plugin_register)(const char *name, void *infostruct);
-
-double NormalizeParm(int parm, double val)
-{
-  if (parm < 0 || parm >= NUM_PARAMS) return 0.0;
-
-  if (param_infos[parm].minval == USE_DB) return DB2VAL(val); // don't clip
-  else if (param_infos[parm].minval == USE_BOOL) val=val>=0.5?1.0:0.0;
-  else val=(val-param_infos[parm].minval)/(param_infos[parm].maxval-param_infos[parm].minval);
-
-  if (val<0.0) return 0.0;
-  if (val>1.0) return 1.0;
-  return val;
-}
-
-double DenormalizeParm(int parm, double val)
-{
-  if (parm < 0 || parm >= NUM_PARAMS) return 0.0;
-
-  if (param_infos[parm].minval == USE_DB) return VAL2DB(val);
-  if (param_infos[parm].minval == USE_BOOL) return val >= 0.5?1.0:0.0;
-
-  val /= param_infos[parm].parm_maxval;
-
-  return val*(param_infos[parm].maxval-param_infos[parm].minval) + param_infos[parm].minval;
-}
-
-static void format_parm(int parm, double val, char *ptr)
-{
-  if (parm < 0 || parm >= NUM_PARAMS) { *ptr=0; return; }
-
-  char tmp[32];
-  sprintf(tmp,"%s%%.%df",(param_infos[parm].minval == USE_DB && val >= 1.0)?"+":"",param_infos[parm].precisiondigits);
-  sprintf(ptr,tmp,DenormalizeParm(parm,val));
-}
-
 
 int reaninjamAccelProc(MSG *msg, accelerator_register_t *ctx)
 {
@@ -179,22 +116,16 @@ public:
   VSTEffectClass(audioMasterCallback cb)
   {
     m_cb=cb;
-    int x;
-    for (x = 0; x < NUM_PARAMS; x ++)
-    {
-      m_parms[x]=NormalizeParm(x,param_infos[x].defparm)*param_infos[x].parm_maxval;
-    }
     memset(&m_effect,0,sizeof(m_effect));
     m_samplerate=44100.0;
     m_hwndcfg=0;
-    configChanged=0;
     m_effect.magic = kEffectMagic;
     m_effect.dispatcher = staticDispatcher;
     m_effect.process = staticProcess;
     m_effect.getParameter = staticGetParameter;
     m_effect.setParameter = staticSetParameter;
-    m_effect.numPrograms=1;
-    m_effect.numParams = NUM_PARAMS;
+    m_effect.numPrograms = 1;
+    m_effect.numParams = 0;
     m_effect.numInputs=NUM_INPUTS;
     m_effect.numOutputs=NUM_OUTPUTS;
 
@@ -207,129 +138,26 @@ public:
     m_effect.ioRatio=1.0;
     m_lasttransportpos=-100000000.0;
     m_lastplaytrackpos=-100000000.0;
-
-    onParmChange();
-
-    Reset();
   }
 
   audioMasterCallback m_cb;
 
-  int configChanged;
 
   ~VSTEffectClass()
   {
     if (m_hwndcfg) DestroyWindow(m_hwndcfg);
   }  
 
-  void Reset()
-  {
-    m_effect.initialDelay = 0;
-  }
-
-  void onParmChange()
-  {
-    //WDL_MutexLock m(&m_mutex);
-
-  }
-  void ProcessEvents(VstEvents *eventlist)
-  {
-//    if (eventlist && eventlist->numEvents)
-  //    m_pelist=eventlist;
-  }
-
-  int GetChunk(void **ptr)
-  {
-    static WDL_Queue m_cfgchunk;
-    m_cfgchunk.Clear();    
-    *ptr = m_cfgchunk.Get();
-    return m_cfgchunk.Available();
-
-  }
-
-  void SetChunk(void *ptr, int size)
-  {
-  }
-
 
   WDL_DLGRET CfgProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
   {
-    static int m_ignore_editmsg;
     switch (uMsg)
     {
       case WM_INITDIALOG:
         m_hwndcfg=hwndDlg;
 
-        {
-          int x;
-          for (x = 0; x < NUM_PARAMS; x ++)
-          {
-            if (param_infos[x].minval != USE_BOOL)
-            {
-              if (param_infos[x].ui_id_slider)
-              {
-                SetWindowLong(GetDlgItem(hwndDlg,param_infos[x].ui_id_slider),0, 1);
-                if (param_infos[x].minval == USE_DB)
-                {
-                  SendDlgItemMessage(hwndDlg,param_infos[x].ui_id_slider,TBM_SETTIC,0,-1);
-                }
-                else
-                {
-                  double dp=NormalizeParm(x,param_infos[x].defparm)*1000.0;
-
-                  if (param_infos[x].slidershape>1.0) dp=sliderscale_sq(dp,1,param_infos[x].slidershape);
-
-                  if (dp<0)dp=0;
-                  else if (dp>1000)dp=1000;
-                  SendDlgItemMessage(hwndDlg,param_infos[x].ui_id_slider,TBM_SETTIC,0,(LPARAM)(dp+0.5));
-                }
-              }
-            }
-          }
-
-        }
-
         ShowWindow(hwndDlg,SW_SHOWNA);
-        configChanged=~0;
       case WM_USER+6606:
-
-        m_ignore_editmsg++;
-        {
-          int cc=configChanged;
-          configChanged=0;
-          int x;
-          for (x = 0; x < NUM_PARAMS; x ++)
-          {
-            if (cc & (1<<x))
-            {
-              if (param_infos[x].minval==USE_BOOL)
-              {
-                CheckDlgButton(hwndDlg,param_infos[x].ui_id_slider,m_parms[x]>=0.5 ? BST_CHECKED:BST_UNCHECKED);
-              }
-              else
-              {
-                if (param_infos[x].ui_id_lbl)
-                {
-                  char buf[512];
-                  format_parm(x,m_parms[x],buf);
-                  SetDlgItemText(hwndDlg,param_infos[x].ui_id_lbl,buf);
-                }
-                if (param_infos[x].ui_id_slider)
-                {
-                  double val;
-                  if (param_infos[x].minval == USE_DB) val=(DB2SLIDER(VAL2DB(m_parms[x])));
-                  else if (param_infos[x].slidershape>1.0) 
-                    val=sliderscale_sq(m_parms[x]*1000.0/param_infos[x].parm_maxval,1,param_infos[x].slidershape);
-                  else val=(m_parms[x]*1000.0)/param_infos[x].parm_maxval;
-                  SendDlgItemMessage(hwndDlg,param_infos[x].ui_id_slider,TBM_SETPOS,0,(LPARAM)val);
-                }
-              }
-            }
-          }
-
-  
-        }
-        m_ignore_editmsg--;
 
       return 0;
       case WM_TIMER:
@@ -343,89 +171,6 @@ public:
             ShowWindow(g_hwnd,SW_SHOWNORMAL);
             SetForegroundWindow(g_hwnd); 
           }
-        }
-        if (!m_ignore_editmsg && HIWORD(wParam) == CBN_SELCHANGE)
-        {
-          int v=SendDlgItemMessage(hwndDlg,LOWORD(wParam),CB_GETCURSEL,0,0);
-          if (v != CB_ERR)
-          {
-            v=SendDlgItemMessage(hwndDlg,LOWORD(wParam),CB_GETITEMDATA,v,0);
-          }
-        }
-        if (!m_ignore_editmsg && HIWORD(wParam) == BN_CLICKED)
-        {
-          int x;
-          for (x = 0; x < NUM_PARAMS; x ++)
-          {
-            if (param_infos[x].minval == USE_BOOL && LOWORD(wParam) == param_infos[x].ui_id_slider)
-            {
-              m_parms[x]=IsDlgButtonChecked(hwndDlg,LOWORD(wParam))?1.0:0.0;
-              onParmChange();
-              if (g_hostcb) g_hostcb(&m_effect,audioMasterAutomate,x,0,NULL,(float)m_parms[x]);
-            }
-          }
-        }
-        if (!m_ignore_editmsg && HIWORD(wParam) == EN_CHANGE)
-        {
-          char buf[512];
-          GetDlgItemText(hwndDlg,LOWORD(wParam),buf,sizeof(buf));
-          double f=atof(buf);
-          int x;
-          for (x = 0; x < NUM_PARAMS; x ++)
-          {
-            if (param_infos[x].ui_id_lbl && param_infos[x].ui_id_lbl == LOWORD(wParam) && param_infos[x].minval != USE_BOOL)
-            {
-              m_parms[x]=NormalizeParm(x,f)*param_infos[x].parm_maxval;
-              if (param_infos[x].ui_id_slider)
-              {
-                double val;
-                if (param_infos[x].minval == USE_DB) val=(DB2SLIDER(VAL2DB(m_parms[x])));
-                else if (param_infos[x].slidershape>1.0) val=sliderscale_sq((m_parms[x]*1000.0)/param_infos[x].parm_maxval,1,param_infos[x].slidershape);
-                else val=(m_parms[x]*1000.0)/param_infos[x].parm_maxval;
-                SendDlgItemMessage(hwndDlg,param_infos[x].ui_id_slider,TBM_SETPOS,0,(LPARAM)val);
-              }
-              onParmChange();
-              if (g_hostcb) g_hostcb(&m_effect,audioMasterAutomate,x,0,NULL,(float)m_parms[x]);
-              break;
-            }
-          }
-        }
- 
-      return 0;
-      case WM_HSCROLL:
-      case WM_VSCROLL:
-        {
-          char buf[512];
-          int pos=SendMessage((HWND)lParam,TBM_GETPOS,0,0);
-          m_ignore_editmsg++;
-
-          int x;
-          for (x = 0; x < NUM_PARAMS; x ++)
-          {
-            if (param_infos[x].ui_id_slider && param_infos[x].minval != USE_BOOL && (HWND)lParam == GetDlgItem(hwndDlg,param_infos[x].ui_id_slider))
-            {
-              if (param_infos[x].minval == USE_DB) m_parms[x]=(float)(DB2VAL(SLIDER2DB((double)pos)));
-              else if (param_infos[x].slidershape>1.0) m_parms[x]=sliderscale_sq(pos,-1,param_infos[x].slidershape)/1000.0 * param_infos[x].parm_maxval;
-              else m_parms[x]=pos/1000.0 * param_infos[x].parm_maxval;
-
-
-              if (param_infos[x].ui_id_lbl)
-              {
-                format_parm(x,m_parms[x],buf);
-                SetDlgItemText(hwndDlg,param_infos[x].ui_id_lbl,buf);
-              }
-
-
-              onParmChange();
-              if (g_hostcb) 
-              {
-                if (LOWORD(wParam)==SB_ENDSCROLL) g_hostcb(&m_effect,audioMasterEndEdit,x,0,NULL,0.0f);
-                else g_hostcb(&m_effect,audioMasterAutomate,x,0,NULL,(float)m_parms[x]);
-              }
-              break;
-            }
-          }
-          m_ignore_editmsg--;
         }
       return 0;
       case WM_DESTROY:
@@ -450,21 +195,11 @@ public:
       case effCanDo:
         if (ptr && !strcmp((char *)ptr,"hasCockosViewAsConfig")) return 0xbeef0000;
         if (ptr && !strcmp((char *)ptr,"hasCockosExtensions")) return 0xbeef0000;
-        if (ptr)
-        {
-          if (//!stricmp((char*)ptr,"sendVstEvents") || 
-              !stricmp((char*)ptr,"sendVstMidiEvent") || 
-              !stricmp((char*)ptr,"receiveVstEvents") || 
-              !stricmp((char*)ptr,"receiveVstMidiEvent"))
-            return 1;
-        }
       return 0;
       case effStopProcess:
       case effMainsChanged:
         if (_this && !value) 
         {
-          WDL_MutexLock m(&_this->m_mutex);
-          _this->Reset();
         }
       return 0;
       case effSetBlockSize:
@@ -496,9 +231,7 @@ public:
       case effSetSampleRate:
         if (_this) 
         {
-          WDL_MutexLock m(&_this->m_mutex);
           _this->m_samplerate=opt;
-          _this->onParmChange();
         }
       return 0;
       case effGetInputProperties:
@@ -556,49 +289,6 @@ public:
       return 0;
       case effOpen:
       return 0;
-      case effProcessEvents:
-        if (ptr && _this)
-          _this->ProcessEvents((VstEvents*)ptr);
-      return 0;
-      case effVendorSpecific:
-        if (ptr && _this && index == 0xdeadbeef+1)
-        {
-          if (value>=0 && value<NUM_PARAMS) 
-          {
-            double *buf=(double *)ptr;  buf[0]=0.0;
-            if (param_infos[value].minval == USE_DB) buf[1]=2.0; else buf[1]=param_infos[value].parm_maxval;
-            return 0xbeef;
-          }
-        }
-        else if (ptr && _this && index == effGetParamDisplay)
-        {
-          if (value>=0 && value<NUM_PARAMS) { format_parm(value,opt,(char *)ptr); return 0xbeef; }
-          else *(char *)ptr = 0;          
-        }
-      return 0;
-      case effGetParamName:
-        if (ptr)
-        {
-          if (index>=0 && index<NUM_PARAMS) lstrcpyn((char *)ptr,param_infos[index].name,9);
-          else *(char *)ptr = 0;
-        }
-      return 0;
-
-      case effGetParamLabel:
-        if (ptr)
-        {
-          if (index>=0 && index<NUM_PARAMS) lstrcpyn((char *)ptr,param_infos[index].label,9);
-          else *(char *)ptr = 0;
-        }
-      return 0;
-
-      case effGetParamDisplay:
-        if (ptr&&_this)
-        {
-          if (index>=0 && index<NUM_PARAMS) format_parm(index,_this->m_parms[index],(char *)ptr);
-          else *(char *)ptr = 0;
-        }
-      return 0;
       case effEditGetRect:
         if (_this)// && _this->m_hwndcfg) 
         {
@@ -615,16 +305,6 @@ public:
           return 1;
 
         }
-      return 0;
-      case effSetChunk:
-        if (_this) 
-        {
-          _this->SetChunk(ptr,value);
-          return 1;
-        }
-      return 0;
-      case effGetChunk:
-        if (_this) return _this->GetChunk((void **)ptr);
       return 0;
       case effIdentify:
         return CCONST ('N', 'v', 'E', 'f');    }
@@ -678,35 +358,24 @@ public:
     }
   }
 
-	static void VSTCALLBACK staticProcess(AEffect *effect, float **inputs, float **outputs, VstInt32 sampleframes)
+  static void VSTCALLBACK staticProcess(AEffect *effect, float **inputs, float **outputs, VstInt32 sampleframes)
   {
 
   }
 
 
-	static void VSTCALLBACK staticSetParameter(AEffect *effect, VstInt32 index, float parameter)
+  static void VSTCALLBACK staticSetParameter(AEffect *effect, VstInt32 index, float parameter)
   {
-    VSTEffectClass *_this = (VSTEffectClass *)effect->object;
-    if (!_this || index < 0 || index >= NUM_PARAMS) return;
-    WDL_MutexLock m(&_this->m_mutex);
-    _this->m_parms[index]=parameter;    
-    _this->onParmChange();
-    _this->configChanged|=1<<index;
   }
 
-	static float VSTCALLBACK staticGetParameter(AEffect *effect, VstInt32 index)
+  static float VSTCALLBACK staticGetParameter(AEffect *effect, VstInt32 index)
   {
-    VSTEffectClass *_this = (VSTEffectClass *)effect->object;
-    if (!_this || index < 0 || index >= NUM_PARAMS) return 0.0;
-    WDL_MutexLock m(&_this->m_mutex);
-    return (float)_this->m_parms[index];
+    return 0.0f;
   }
 
   HWND m_hwndcfg;
   double m_samplerate;
   AEffect m_effect;
-  double m_parms[1 /*NUM_PARAMS*/];
-  WDL_Mutex m_mutex;
 
   double m_lasttransportpos,m_lastplaytrackpos;
 
